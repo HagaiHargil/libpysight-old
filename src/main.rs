2b810aee332a13991357709126826e137e66f839
Hagai Hargil <hagaihargil@gmail.com>
2017-04-19 11:16:09 +0300

    Split channels to different HashMap keys

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,22 @@ extern crate rread_lst;
 fn main() {
     let data = rread_lst::reading::main();
 
-    println!("Channel: {:b}", data[0].channel);
-    println!("Edge: {:?}", data[0].edge);
-    println!("Sweep: {:b}", data[0].sweep);
-    println!("TAG: {:b}", data[0].tag);
-    println!("Time: {:b}", data[0].time);
-    println!("Num of entries: {:?}", data.len())
+    //    let mut counter: u64 = 0;
+//    let mut idx: u64 = 0;
+//    let mut vec_of_sweeps = Vec::new();
+//
+//    for line in &data {
+//        if line.sweep == 0 {
+//            counter += 1;
+//            vec_of_sweeps.push((line.sweep, idx, line.time, line.lost, line.tag, line.channel, line.edge));
+//        }
+//        idx += 1;
+//    }
+//
+//    println!("Counter: {}", counter);
+//    println!("Vec: {:?}, {:?}, {:?}, {:?}", vec_of_sweeps[0],
+//    vec_of_sweeps[1], vec_of_sweeps[2], vec_of_sweeps[3]);
+//    println!("Gen: {}, {}, {}", data[10].time, data[10].sweep, data[10].channel)
+
 
 }
--- src/reading/mod.rs
+++ src/reading/mod.rs
@@ -8,18 +8,18 @@ use bitreader::BitReader;
 pub struct DataLine {
     pub lost: u8,
     pub tag: u16,
-    pub channel: u8,
+//    pub channel: u8,
     pub edge: bool,
     pub sweep: u16,
     pub time: u64,
 }
 
-pub fn main() -> Vec<DataLine> {
-    // let file_path = r"C:\Users\Hagai\Documents\GitHub\rread_lst\TAG lens in fixed sample 3 - 62p 188kHz - zoom 4x - single 65.6 milliseconds sweep - 8 byte words - 1200 um ABOVE SAMPLE - 200 fps - unlocked to scanimage - BINARY LISTFILE - 300 mV threshold - 011.lst";
-    let file_path = r"C:\Users\Hagai\Documents\GitHub\rread_lst\TAG lens in fixed sample 3 - 62p 188kHz - zoom 4x - single 10 milliseconds sweep - 8 byte words - 130 um DEEP - 200 fps - unlocked to scanimage - BINARY LISTFILE - 300 mV threshold - 009.lst";
-    let start_of_data_pos: usize = 1572;
+pub fn main() -> HashMap<u8, Vec<DataLine>> {
+    let file_path = r"C:\Users\Hagai\Documents\GitHub\rread_lst\TAG lens in fixed sample 3 - 62p 188kHz - zoom 4x - single 65.6 milliseconds sweep - 8 byte words - 1200 um ABOVE SAMPLE - 200 fps - unlocked to scanimage - BINARY LISTFILE - 300 mV threshold - 013.lst";
+    // let file_path = r"C:\Users\Hagai\Documents\GitHub\rread_lst\TAG lens in fixed sample 3 - 62p 188kHz - zoom 4x - single 10 milliseconds sweep - 8 byte words - 130 um DEEP - 200 fps - unlocked to scanimage - BINARY LISTFILE - 300 mV threshold - 009.lst";
+    let start_of_data_pos: usize = 1578;
     let range = 8u64;
-    let timepatch = String::from("5b");
+    let timepatch = String::from("f3");
     let returned_data_from_bytes = read_file_as_bytes(file_path, start_of_data_pos,
                                                       timepatch.as_str(), range);
 
@@ -27,7 +27,7 @@ pub fn main() -> Vec<DataLine> {
 }
 
 fn read_file_as_bytes(file_path: &str, start_of_data_pos: usize,
-                      timepatch: &str, range: u64) -> Vec<DataLine> {
+                      timepatch: &str, range: u64) -> HashMap<u8, Vec<DataLine>> {
 
     // Open the file and go to start-of-data position
     let data_including_headers = FileBuffer::open(file_path).unwrap();
@@ -60,7 +60,6 @@ fn create_timepatch_map<'a>() -> HashMap<&'a str, [u8; 4]> {
     timepatch_map.insert("32", array_for_32);
 
     let array_for_f3 = [1, 16, 7, 36];
-    //let array_for_f3 = [0, 0, 0, 0];
     timepatch_map.insert("f3", array_for_f3);
 
     let array_for_5b = [1, 15, 16, 28];
@@ -69,218 +68,75 @@ fn create_timepatch_map<'a>() -> HashMap<&'a str, [u8; 4]> {
     timepatch_map
 }
 
+fn create_channel_map(data_size: usize) -> HashMap<u8, Vec<DataLine>>{
+    let mut channel_map = HashMap::new();
+
+    for idx in 0..8 {
+        channel_map.insert(idx as u8, Vec::with_capacity(data_size + 1));
+    }
+
+    channel_map
+}
+
 fn iterate_over_f3(data: &[u8], range: u64, bit_order: &[u8; 4], chunk_size: usize,
-                     data_size: usize) -> Vec<DataLine> {
-    let mut data_processed: Vec<DataLine> = Vec::with_capacity(data_size + 1);
+                     data_size: usize) -> HashMap<u8, Vec<DataLine>> {
     let mut lost: u8;
     let mut tag: u16;
     let mut sweep: u16;
     let mut time: u64;
     let mut edge: bool;
-    let mut channel: u8;
-
-    println!("chunk_size: {}, data_size: {}, data/chunk: {}", chunk_size, data_size,
-             data_size/chunk_size);
-    println!("Making sure we didn't lose anyone: {}", chunk_size * (data_size/chunk_size));
-
-    let first_row = &data[0..chunk_size];
-    println!("first row: {:?}", first_row);
-    let second_row = &data[chunk_size..chunk_size*2];
-    println!("second row: {:?}", second_row);
-    let third_row = &data[chunk_size*2..chunk_size*3];
-    println!("third row: {:?}", third_row);
-    let fourth_row = &data[chunk_size*3..chunk_size*4];
-    println!("fourth row: {:?}", fourth_row);
-
-    let mut reader_f = BitReader::new(first_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(second_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(third_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(fourth_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-//    let bits_lost = reader_f.read_u64(1).unwrap();
-//    let bits_tag = reader_f.read_u64(15).unwrap();
-//    println!("Bits_lost: {:b}", bits_lost);
-//    println!("Bits_tag: {:b}", bits_tag);
+    let mut reversed_vec = Vec::with_capacity(chunk_size + 1);
+    let mut map_of_data = create_channel_map(data_size);
 
     for cur_data in data.chunks(chunk_size) {
-        let mut reader = BitReader::new(cur_data);
+        reversed_vec.truncate(0);
+        reversed_vec.extend(cur_data.iter().rev());
+        let mut reader = BitReader::new(&reversed_vec);
         tag = reader.read_u16(bit_order[1]).unwrap();
         lost = reader.read_u8(bit_order[0]).unwrap();
         sweep = reader.read_u16(bit_order[2]).unwrap();
         time = reader.read_u64(bit_order[3]).unwrap();
         edge = reader.read_bool().unwrap();
-        channel = reader.read_u8(3).unwrap();
 
-        time = time  + (range * (sweep as u64)); // TODO: should be -1
+        time = time + range * ((sweep) as u64); // TODO: should be -1
 
-        data_processed.push(DataLine { channel: channel, edge: edge, sweep: sweep,
-                                       time: time, tag: tag, lost: lost });
+        // Populate a hashmap, each key being an input channel and the values are a vector
+        // of DataLines
+        map_of_data.get_mut(&reader.read_u8(3).unwrap()).unwrap()
+            .push(DataLine { edge: edge, sweep: sweep,
+                             time: time, tag: tag, lost: lost });
     }
-    data_processed
-
+    map_of_data
 }
 
 fn iterate_over_file(data: &[u8], range: u64, bit_order: &[u8; 4], chunk_size: usize,
-                     data_size: usize) -> Vec<DataLine> {
-    let mut data_processed: Vec<DataLine> = Vec::with_capacity(data_size + 1);
+                     data_size: usize) -> HashMap<u8, Vec<DataLine>> {
+
     let mut lost: u8;
     let mut tag: u16;
     let mut sweep: u16;
     let mut time: u64;
     let mut edge: bool;
-    let mut channel: u8;
-
-    println!("chunk_size: {}, data_size: {}, data/chunk: {}", chunk_size, data_size,
-             data_size/chunk_size);
-    println!("Making sure we didn't lose anyone: {}", chunk_size * (data_size/chunk_size));
-    let first_row = &data[0..chunk_size];
-    println!("first row: {:?}", first_row);
-    let second_row = &data[chunk_size..chunk_size*2];
-    println!("second row: {:?}", second_row);
-    let third_row = &data[chunk_size*2..chunk_size*3];
-    println!("third row: {:?}", third_row);
-    let fourth_row = &data[chunk_size*3..chunk_size*4];
-    println!("fourth row: {:?}", fourth_row);
-
-    let mut reader_f = BitReader::new(first_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(second_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(third_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-    let mut reader_f = BitReader::new(fourth_row);
-    let all_bits = reader_f.read_u64(64).unwrap();
-    println!("All bits 1: {:b}", all_bits);
-//    let bits_lost = reader_f.read_u64(1).unwrap();
-//    let bits_tag = reader_f.read_u64(15).unwrap();
-//    println!("Bits_lost: {:b}", bits_lost);
-//    println!("Bits_tag: {:b}", bits_tag);
+    let mut reversed_vec = Vec::with_capacity(chunk_size + 1);
+    let mut map_of_data = create_channel_map(data_size);
 
     for cur_data in data.chunks(chunk_size) {
-        let mut reader = BitReader::new(cur_data);
+        reversed_vec.truncate(0);
+        reversed_vec.extend(cur_data.iter().rev());
+        let mut reader = BitReader::new(&reversed_vec);
         lost = reader.read_u8(bit_order[0]).unwrap();
         tag = reader.read_u16(bit_order[1]).unwrap();
         sweep = reader.read_u16(bit_order[2]).unwrap();
         time = reader.read_u64(bit_order[3]).unwrap();
         edge = reader.read_bool().unwrap();
-        channel = reader.read_u8(3).unwrap();
+        time = time  + (range * ((sweep) as u64)); // TODO: should be -1
 
-        time = time  + (range * (sweep as u64)); // TODO: should be -1
-
-        data_processed.push(DataLine { channel: channel, edge: edge, sweep: sweep,
-                                       time: time, tag: tag, lost: lost });
+        // Populate a hashmap, each key being an input channel and the values are a vector
+        // of DataLines
+        map_of_data.get_mut(&reader.read_u8(3).unwrap()).unwrap()
+            .push(DataLine { edge: edge, sweep: sweep,
+                             time: time, tag: tag, lost: lost });
     }
-    data_processed
+    map_of_data
 }
-
-//    loop {
-//        match data.next_bytes() {
-//            csv::NextField::EndOfCsv => break,
-//            csv::NextField::EndOfRecord => {},
-//            csv::NextField::Error(err) => panic!(err),
-//            csv::NextField::Data(row) => {
-//                let mut reader = bitreader::BitReader::new(row);
-//                //channel = reader.read_u8(3).unwrap();
-//                channel = 1;
-//                println!("row: {:?}", row);
-//                edge = reader.read_bool().unwrap();
-//                time = reader.read_u64(bit_order[3]).unwrap();
-//                lost = reader.read_u8(bit_order[0]).unwrap();
-//                sweep = reader.read_u16(bit_order[2]).unwrap();
-//                tag = reader.read_u16(bit_order[1]).unwrap();
-//                time = time  + (range * (sweep as u64)); // TODO: should be -1
-//
-//                data_processed.push(DataLine { channel: channel, edge: edge, sweep: sweep,
-//                                               time: time, tag: tag, lost: lost })
-//            }
-//        }
-//    }
-
-//pub fn read_file_as_str() -> Vec<DataLine> {
-//    // Read the filename as an ASCII file
-//
-//    let file_path = r"C:\Users\Hagai\Documents\GitHub\Multiscaler_Image_Generator\PMT1_Readings_one_sweep_equals_one_frame.lst";
-//    let start_of_data_pos: u64 = 1546;
-//    const NUM_OF_LINES: usize = 10000;
-//
-//    let mut all_bin: &str = "1";
-//    let hex_to_bin: HashMap<char, &'static str> = create_hash_map_1d();
-//    let mut time: u64 = 1;
-//
-//    let mut data_processed = Vec::with_capacity(NUM_OF_LINES);
-//    let mut f = File::open(file_path).unwrap();
-//    let _ = f.seek(SeekFrom::Start(start_of_data_pos));
-//
-//    let mut data = csv::Reader::from_reader(f).has_headers(false);
-//    for mut row in data.records().map(|r| r.unwrap()) {
-//        // Code block #1
-//        let all_bin = match hex_to_bin.get(&row[0].pop().unwrap()) {
-//            Some(bin_value) => bin_value,
-//            None => "9876"
-//        };
-//        let (edge, channel) = all_bin.split_at(1);
-//
-//        // Code block #2
-//        // let (lost, sweep) = get_lost_sweep(&row[0][0..2]);
-//
-//
-//        data_processed.push(DataLine {channel: channel.to_string(), edge: edge.to_string(), sweep: 1, time: 1 });
-//    }
-//    data_processed
-//}
-//
-//fn create_hash_map_1d<'a>() -> HashMap<char, &'a str> {
-//    let vec_of_letters = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-//    let mut hex_to_bin_map = HashMap::new();
-//    let mut num = 0;
-//    let mut midway = "a".to_string();
-//    for (idx, hex_let1) in (&vec_of_letters).enumerate() {
-//        midway = format!("{:04b}", idx);
-//        hex_to_bin_map.insert(hex_let1, midway);
-//    }
-//
-//    //let hex_to_bin_map = hex_to_bin_map;
-//    hex_to_bin_map
-//
-//}
-
-//fn create_hash_map_2d() -> HashMap<String, String> {
-//    let vec_of_letters = vec!['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-//    let mut hex_to_bin_map = HashMap::new();
-//    let mut num = 0;
-//    let mut midway = "a".to_string();
-//
-//    for hex_let1 in &vec_of_letters {
-//        for hex_let2 in &vec_of_letters {
-//            midway = format!("{:08b}", num);
-//            hex_to_bin_map.insert(hex_let1.to_string() + &hex_let2.to_string(), midway);
-//            num = num + 1;
-//        }
-//
-//    }
-//    let hex_to_bin_map = hex_to_bin_map;
-//    hex_to_bin_map
-//}
-
-
-//fn get_lost_sweep<'a>(row: &'a str) -> (String, u16) {
-//    /// get a 2-byte &str from the .lst file and return the sweep and data lost bits
-//
-//    let bin_bytes: String = format!(&"{:b}", row);
-//    let num: u16 = 2;
-//
-//    (bin_bytes, num)
-//
-//}
-//
 
